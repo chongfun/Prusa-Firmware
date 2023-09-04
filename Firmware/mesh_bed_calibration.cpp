17b4eab77f56e861b9b754e03df6d34bfb68b57d
Kyle Chong <kyle@chongfun.com>
2023-09-03 22:49:29 -0700

    Align mesh bed points with my heatbed

--- Firmware/mesh_bed_calibration.cpp
+++ Firmware/mesh_bed_calibration.cpp
@@ -53,22 +53,22 @@ const float bed_skew_angle_extreme = (0.25f * M_PI / 180.f);
 const float bed_ref_points[] PROGMEM = {
     13.f  - BED_ZERO_REF_X,   6.4f - BED_ZERO_REF_Y,
     115.f - BED_ZERO_REF_X,   6.4f - BED_ZERO_REF_Y,
-    216.f - BED_ZERO_REF_X,   6.4f - BED_ZERO_REF_Y,
+    217.f - BED_ZERO_REF_X,   6.4f - BED_ZERO_REF_Y,
 
-    216.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
+    217.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
     115.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
     13.f  - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
 
     13.f  - BED_ZERO_REF_X, 202.4f - BED_ZERO_REF_Y,
     115.f - BED_ZERO_REF_X, 202.4f - BED_ZERO_REF_Y,
-    216.f - BED_ZERO_REF_X, 202.4f - BED_ZERO_REF_Y
+    217.f - BED_ZERO_REF_X, 202.4f - BED_ZERO_REF_Y
 };
 
 // Positions of the bed reference points in the machine coordinates, referenced to the P.I.N.D.A sensor.
 // The points are the following: center front, center right, center rear, center left.
 const float bed_ref_points_4[] PROGMEM = {
     115.f - BED_ZERO_REF_X,   6.4f - BED_ZERO_REF_Y,
-    216.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
+    217.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
     115.f - BED_ZERO_REF_X, 202.4f - BED_ZERO_REF_Y,
     13.f  - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y
 };
